d2ba1f0d43cc1370f489b5e29517cd664a1c86b3
Ethan Joiner <ethan.joiner@gmail.com>
2019-11-20 13:53:48 -0600

    variable exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,6 +8,32 @@ public class HelloWorld {
 
             String myString = "Howdy";
             System.out.println(myString);
+
+            float myNumber = 3.14f;
+            System.out.println(myNumber);
+
+//            int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+
+//            String theNumberThree = "three";
+//            Object o = theNumberThree;
+//            int three = (int) o;
+//            int three = (int) "three";
+
+//            int x = 4;
+//            x += 5;
+
+//            int x = 3;
+//            int y = 4;
+//            y *= x;
+
+//            int x = 10;
+//            int y = 2;
+//            x /= y;
+//            y -= x;
+
+//            long one = 1;
         }
     }
 
