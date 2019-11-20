cc7585a3f3af074d7513e37f3f0fbc72fa9bbce1
Ethan Joiner <ethan.joiner@gmail.com>
2019-11-20 13:35:11 -0600

    part 2 done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,13 @@
 public class HelloWorld {
         public static void main(String[] args) {
             System.out.print("Hello, ");
-            System.out.print("World!");
+            System.out.println("World!");
+
+            int myFavoriteNumber = 17;
+            System.out.println(myFavoriteNumber);
+
+            String myString = "Howdy";
+            System.out.println(myString);
         }
     }
 
