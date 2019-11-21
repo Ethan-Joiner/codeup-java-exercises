9268eb1ef6314bd3033ef06ab4b53e2f03cbdcac
Ethan Joiner <ethan.joiner@gmail.com>
2019-11-21 14:35:56 -0600

    table done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,15 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (byte i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("Fizzbuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int input = sc.nextInt();
+        System.out.printf("What number would you like to go up to? %d%n%n", input);
+        System.out.println("Here is your table!\n");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for (int i = 1; i <= input; i++) {
+            System.out.printf("%d      | %d       | %d\n", i, (i * i), (i * i * i));
         }
     }
 }
