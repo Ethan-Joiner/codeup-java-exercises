6a816414915a11f3b3c285f28a1470354d9a73b8
Ethan Joiner <ethan.joiner@gmail.com>
2019-11-22 10:29:05 -0600

    turned into bad friend simulator

--- src/Bob.java
+++ src/Bob.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    String input = sc.nextLine();
-    if (input.endsWith("?")) {
-        System.out.println("Sure");
-    } else if (input.endsWith("!")) {
-        System.out.println("Whoa, chill out!");
-    } else if (input.equalsIgnoreCase("")) {
-        System.out.println("Fine. Be that way!");
-    } else {
-        System.out.println("Whatever");
-    }
+        String input = sc.nextLine();
+        while (true) {
+            if (input.endsWith("?")) {
+                System.out.println("Sure");
+            } else if (input.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (input.equalsIgnoreCase("")) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever");
+            }
+            input = sc.nextLine();
+        }
     }
 }
