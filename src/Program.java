0384646f55a00a4c5e8d0f4575a223b8b11c1aca
Ethan Joiner <ethan.joiner@gmail.com>
2019-12-04 13:42:36 -0600

    another edabit

--- src/Program.java
+++ src/Program.java
@@ -43,4 +43,14 @@ public class Program {
         }
         return equal;
     }
+
+    public static String removeVowels(String s) {
+        String noVowel = "";
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) != 'a' && s.charAt(i) != 'e' && s.charAt(i) != 'i' && s.charAt(i) != 'o' && s.charAt(i) != 'u' && s.charAt(i) != 'A' && s.charAt(i) != 'E' && s.charAt(i) != 'I' && s.charAt(i) != 'O' && s.charAt(i) != 'U'){
+                noVowel += s.charAt(i);
+            }
+        }
+            return noVowel;
+    }
 }
