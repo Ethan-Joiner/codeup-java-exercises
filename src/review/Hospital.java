b894e052d887a2d8961880c2dcfa0ebd8d355d29
Ethan Joiner <ethan.joiner@gmail.com>
2019-12-11 13:54:29 -0600

    exception review

---
+++ src/review/Hospital.java
@@ -0,0 +1,24 @@
+package review;
+
+public class Hospital {
+    public static void main(String[] args) {
+
+
+        try {
+            makeApt(null);
+            makeApt("2019-12-11");
+        } catch (Exception e) {
+            e.printStackTrace();
+//            handle: writes, in logs, send email
+//            main(args);
+        }
+    }
+
+    private static void makeApt(String date) throws Exception{
+        if(date == null){
+            throw new Exception("We cannot work with null dates");
+        } else if (date < today){
+            throw new Exception("We cannot work with null dates");
+        }
+    }
+}
