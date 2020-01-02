04c0824691ad902537b393586ca2594a738ca469
Ethan Joiner <ethan.joiner@gmail.com>
2020-01-02 15:04:02 -0600

    28th complete

---
+++ src/Christmas/Santa.java
@@ -0,0 +1,10 @@
+package Christmas;
+
+import java.util.Scanner;
+public class Santa {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+    System.out.println("What would you like for Christmas?");
+    String request = sc.nextLine();
+    }
+}
