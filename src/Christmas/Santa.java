229cc2896daac0b67ab2149410d5e909d7f6296e
Ethan Joiner <ethan.joiner@gmail.com>
2020-01-02 15:07:01 -0600

    28 bonus complete

--- src/Christmas/Santa.java
+++ src/Christmas/Santa.java
@@ -2,9 +2,16 @@ package Christmas;
 
 import java.util.Scanner;
 public class Santa {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-    System.out.println("What would you like for Christmas?");
-    String request = sc.nextLine();
+        Boolean flag = true;
+        while (flag) {
+            System.out.println("What would you like for Christmas?");
+            String request = sc.nextLine();
+            if (request.length() > 2) {
+                flag = false;
+            }
+        }
     }
+
 }
