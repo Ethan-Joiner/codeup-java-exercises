892aa81b4427e2c32ba9941f5dbbc72d555c5045
Ethan Joiner <ethan.joiner@gmail.com>
2020-03-18 22:29:49 -0500

    ez

--- src/Program.java
+++ src/Program.java
@@ -336,6 +336,10 @@ public class Program {
         /*
          * Write your code here.
          */
-
+        int sum = 0;
+        for (int i = 0; i < ar.length; i++) {
+            sum += ar[i];
+        }
+        return sum;
     }
 }
