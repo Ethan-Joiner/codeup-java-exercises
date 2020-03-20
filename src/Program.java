a9ff4b6497e4fed8838625098b23f2058168e56c
Ethan Joiner <ethan.joiner@gmail.com>
2020-03-19 21:05:06 -0500

    this website is tedious. how am I supposed to know what isn't working when the test cases are locked

--- src/Program.java
+++ src/Program.java
@@ -419,7 +419,7 @@ public class Program {
         int blown = 1;
         Arrays.sort(ar);
         for (int i = ar.length -2; i > 0; i--) {
-            if (ar[i] == ar[i - 1]) {
+            if (ar[i] == ar[i + 1]) {
                 blown ++;
             }
         }
