cc200bfc875f2ca1de58cb48992ac123224d6601
Ethan Joiner <ethan.joiner@gmail.com>
2020-03-19 19:54:39 -0500

    am i thinking too simply

--- src/Program.java
+++ src/Program.java
@@ -399,4 +399,20 @@ public class Program {
         }
     }
 
+//    Take an array of ints and return the minimum and maximum sums from the numbers provided
+    static void miniMaxSum(int[] arr) {
+        int min = 0;
+        int max = 0;
+        Arrays.sort(arr);
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            min += arr[i];
+        }
+        for (int i = 1; i < arr.length - 1; i++) {
+            min += arr[i];
+        }
+
+
+    }
+
 }
