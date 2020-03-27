4938e5ac2cc5e66764276dff54912bbca5dbed1e
Ethan Joiner <ethan.joiner@gmail.com>
2020-03-26 22:43:29 -0500

    should be fun

---
+++ src/HackerRank.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class HackerRank {
+    public static void main(String[] args) {
+
+    }
+
+//    Take in an array of grades and round up if passing
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        // Write your code here
+
+    }
+}
