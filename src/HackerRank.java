1f398efe201a52f50fecdf6a6d4d18218cd4408d
Ethan Joiner <ethan.joiner@gmail.com>
2020-04-22 23:52:19 -0500

    passed

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -127,7 +127,7 @@ public class HackerRank {
     }
 
 //    Take in an array and an int. Find how many pairs in the array are evenly divisible by k
-//    Return to this. Need to find on the site
+//    Pretty sure this is passed
     static int divisibleSumPairs(int n, int k, int[] ar) {
         int total = 0;
         for (int i = 0; i < n; i++) {
