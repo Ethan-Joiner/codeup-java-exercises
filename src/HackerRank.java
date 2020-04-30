10fa41eb5fe332e975185faa6045f811617f213a
Ethan Joiner <ethan.joiner@gmail.com>
2020-04-29 23:02:06 -0500

    easy

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -101,7 +101,7 @@ public class HackerRank {
     }
 
 //    S is array of numbers to be added, d is total expected, m is how many touching can be considered
-//    May need to delete and start over. Find on site for refresher. Simple fix. Cant next loops
+//    May need to delete and start over. Dont nest
     static int birthday(List<Integer> s, int d, int m) {
         int total = 0;
         int counter = 0;
