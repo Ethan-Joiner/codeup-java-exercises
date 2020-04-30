aa8023c45d78fdfc423958c8421f62c2adc02e72
Ethan Joiner <ethan.joiner@gmail.com>
2020-04-29 23:01:41 -0500

    you got it

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -78,7 +78,7 @@ public class HackerRank {
 //    }
 
 //    Tally times highest and lowest are reached from array
-//    Not sure what the problem is, still failing a couple tests.
+//    Not sure what the problem is, still failing a couple tests. Double check
     static int[] breakingRecords(int[] scores) {
         int min = scores[0];
         int max = scores[0];
