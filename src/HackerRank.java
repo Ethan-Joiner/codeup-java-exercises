a222b6a9dbc5e9c8b154b183ccd8085e3f87ce6e
Ethan Joiner <ethan.joiner@gmail.com>
2020-05-07 18:02:05 -0500

    passed all tests

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -136,7 +136,7 @@ public class HackerRank {
     }
 
 //    Take in a list of items charged (bill), add together and remove index of k, if equals B correct, if not print difference
-//    Passed 10/12, needs review. Check your math. Make sure to cut excess
+// Passed all tests
     static void bonAppetit(List<Integer> bill, int k, int b) {
         int total = 0;
         for (int item: bill) {
