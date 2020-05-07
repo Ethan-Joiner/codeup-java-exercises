99fbfddb997f86ce77722f83ae71defbddb39c1f
Ethan Joiner <ethan.joiner@gmail.com>
2020-05-07 16:54:07 -0500

    migrator birds set up

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -259,3 +259,12 @@ public class HackerRank {
     }
 
 }
+
+// For numbers 1 through 5, find which shows up the most times. If there is a tie, return the lower number
+static int migratoryBirds(List<Integer> arr) {
+	winnerBird = 0;
+	
+	return winnerBird;
+
+
+}
