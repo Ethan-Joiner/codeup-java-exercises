8f0584f59ed06b3c2bbbc33019ae1b2aa4ebdea9
Ethan Joiner <ethan.joiner@gmail.com>
2020-05-08 17:58:23 -0500

    and the testing begins

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -8,7 +8,8 @@ public class HackerRank {
 //        int[] test = {2,5,1,3,4,4,3,5,1,1,2,1,4,1,3,3,4,2,1};
 //        ArrayList<Integer> testing = new ArrayList<>(Arrays.asList(2,5,1,3,4,4,3,5,1,1,2,1,4,1,3,3,4,2,1));
 //        System.out.println(birthday(testing,18,7));
-        System.out.println(findDigits(123456789));
+//        System.out.println(findDigits(123456789));
+    	System.out.println(saveThePrisoner(3,7,3,));
     }
 
 
