0ae48b998e3be04697ed17e5b5478ed16ca8f278
Ethan Joiner <ethan.joiner@gmail.com>
2020-05-14 22:35:29 -0500

    how to do this more efficiently?

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -284,7 +284,7 @@ public class HackerRank {
 
 
 // For numbers 1 through 5, find which shows up the most times. If there is a tie, return the lower number
-//    Incomplete. 
+//    Incomplete. Must be a faster way than a massive switch
 //static int migratoryBirds(List<Integer> arr) {
 //	 int winnerBird = 0;
 //	 int oneCount = 0;
