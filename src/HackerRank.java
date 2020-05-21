267e7735566159721818a687f088fb1558dcc1b9
Ethan Joiner <ethan.joiner@gmail.com>
2020-05-20 23:27:28 -0500

    stubborn

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -381,12 +381,14 @@ static int minimumDistances(int[] a) {
 // This all looks wrong. Try again tomorrow. 
 static int howManyGames(int p, int d, int m, int s) {
 	int games = 1;
-	int price = p - d;
+	int price = p;
 	int spent = p;
     // Return the number of games you can buy
 	for (int i = s - p; i >= price; i -= price) {
-		System.out.println(i);
+		System.out.println("Money" + i);
 		System.out.println("Spent " + spent);
+		System.out.println("Price " + price);
+		System.out.println("Games " + games);
 		
 		if (price > m && price - d >= m) {
 			price -= d;
