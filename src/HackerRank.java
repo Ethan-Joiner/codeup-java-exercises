a56a2e6cb1cedd883f3483f863b651b39ac143d1
Ethan Joiner <ethan.joiner@gmail.com>
2020-05-27 23:05:07 -0500

    string conversion is too heavy

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -250,7 +250,7 @@ public class HackerRank {
 //    }
 
 //    I is start day, j is last day. Test each day by subtracting it from its reverse. If evenly divisible by K then increase days
-//    Passed initial tests, runtime error for larger ones. Need better way to reverse
+//    Passed initial tests, runtime error for larger ones. Need better way to reverse. Try the formula
 //    static int beautifulDays(int i, int j, int k) {
 //        int days = 0;
 //        String reverseOne = "";
