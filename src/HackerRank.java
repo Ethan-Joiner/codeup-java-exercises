836f80403a038469e6b78e0543fc2442f8fc4ca8
Ethan Joiner <ethan.joiner@gmail.com>
2020-05-27 23:04:14 -0500

    requires a formula

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -317,7 +317,7 @@ static int findDigits(int n) {
 }
 
 // n is prisoners, m is pieces of candy, s is starting position. go in a "circle" until you land at the end and return that number
-// Incomplete. Getting runtime error. Needs to run faster.
+// Incomplete. Getting runtime error. Needs to run faster. Probably a formula for it
 static int saveThePrisoner(int n, int m, int s) {
 	int start = s;
 	for (int i = 1; i < m; i ++) {
