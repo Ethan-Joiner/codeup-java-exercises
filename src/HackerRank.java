4998d6e57d29aea12d2cb728f9990a3a4ffa1802
Ethan Joiner <ethan.joiner@gmail.com>
2020-06-03 23:48:37 -0500

    is it bad to assign extra variables? do some research

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -468,7 +468,11 @@ static void cardDeck() {
 // Take the total sum of both diagonals of a 2d matrix, and return the absolute difference
 public static int diagonalDifference(List<List<Integer>> arr) {
 	int difference = 0;
+	int sum1 = 0;
+	int sum2 = 0;
     // Write your code here
+	
+	
 
 	return difference;
     }
