9d822cea3c427d5945354b986bc6749e80e7f8ce
Ethan Joiner <ethan.joiner@gmail.com>
2020-06-08 23:12:39 -0500

    confusing myself, made test list for visualization

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -480,10 +480,15 @@ public static int diagonalDifference(List<List<Integer>> arr) {
 // Take an array of numbers. Return the one that shows up the most. If tied, show the lowest number.
 static int migratoryBirds(List<Integer> arr) {
 	int mostBird = 0;
-	List<Integer> mostBirds = new ArrayList<>();
+	int currentNumber = 0;
+	int currentBird = 0;
+	
+//	List<Integer> test = new ArrayList<>(Arrays.asList(1,1,1,2,2,3,3,4,4,4));
+
 	Collections.sort(arr);
 	for (int i = 0; i < arr.size(); i++) {
-		
+		currentBird = arr.get(i);
+		currentNumber = arr.lastIndexOf(arr.get(i)) - arr.get(i) + 1;
 	}
 	
 	return mostBird;
