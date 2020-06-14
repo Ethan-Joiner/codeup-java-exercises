06390c253747ecdafb3a77edd0d33a8a80bb417e
Ethan Joiner <ethan.joiner@gmail.com>
2020-06-13 21:42:41 -0500

    Now passing one test but miserably failing another

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
 
 public class HackerRank {
     private static final int Infinity = 0;
@@ -15,7 +14,7 @@ public class HackerRank {
 //        int[] test = {2,5,1,3,4,4,3,5,1,1,2,1,4,1,3,3,4,2,1};
 //    	int[] test = {1,1};
     	// List<List<Integer> >test = new ArrayList<>(Arrays.asList(new ArrayList<>(Arrays.asList(11,2,4)),new ArrayList<>(Arrays.asList(4,5,6)),new ArrayList<>(Arrays.asList(10,8,-12))));
-		List<Integer> test = new ArrayList<>(Arrays.asList(1,2,2,3,1,2));
+		List<Integer> test = new ArrayList<>(Arrays.asList(4,6,5,3,1,3));
     
 //        ArrayList<Integer> testing = new ArrayList<>(Arrays.asList(2,5,1,3,4,4,3,5,1,1,2,1,4,1,3,3,4,2,1));
 //        System.out.println(birthday(testing,18,7));
@@ -551,7 +550,7 @@ public static int pickingNumbers(List<Integer> a) {
 	int startingPoint = a.get(0);
 
 	for (int i = 0; i < a.size() - 1; i++) {
-		if (a.get(i + 1) - startingPoint <= 1){
+		if (a.get(i) - startingPoint <= 1){
 			currentLongest++;
 			System.out.println("Current " + currentLongest);
 			System.out.println("Number " + a.get(i));
