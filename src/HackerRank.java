55f6da4cd9f342301c336fb12110cd81e3d4d90f
Ethan-Joiner <ethan.joiner@gmail.com>
2021-06-21 14:43:31 -0500

    handshake

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -726,6 +726,12 @@ public static int lowestTriangle(int trianglebase, int area) {
 	
 		}
 
+		// N is the number of people. How many handshakes need to take place for each person to shake each other's hand once?
+		public static int handshake(int n) {
+			// Write your code here
+		
+			}
+
 }
 
 
