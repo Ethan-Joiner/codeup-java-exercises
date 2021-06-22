731f0a7795117f7a1a4656f50a07425f27c2523f
Ethan-Joiner <ethan.joiner@gmail.com>
2021-06-22 14:01:56 -0500

    testing

--- src/util/warmup.js
+++ src/util/warmup.js
@@ -186,7 +186,14 @@ return length;
 
 // Find the fine based on due date versus return date
 function libraryFine(d1, m1, y1, d2, m2, y2) {
-    // Write your code here
-
+    let fine = 0;
+    if(y1 > y2) {
+        fine = (y1 - y2) * 10000;
+    } else if (m1 > m2 && y1 == y2) {
+        fine = (m1 - m2) * 500;
+    } else if (d1 > d2 && m1 == m2 && y1 == y2){
+        fine = (d1 - d2) * 15;
+    }
+    return fine;
 }
 
