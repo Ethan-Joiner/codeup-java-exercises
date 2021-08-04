9c1b14a807278d434dab4908a0dd950ab76d22b5
Ethan-Joiner <ethan.joiner@gmail.com>
2021-08-04 12:03:00 -0500

    problem appears to be bugged on the site, will return to later

--- src/util/warmup.js
+++ src/util/warmup.js
@@ -588,7 +588,7 @@ function climbingLeaderboard(ranked, player) {
 // }
 // nonDivisibleSubset(7,[278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436]);
 
-// ACM TEAM
+// ACM TEAM - Problem Bugged
 
 function acmTeam(topic) {
     let answer = [];
