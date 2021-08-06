5c18a703cd1882bfec4ed36c2ebf4d62f7459de9
Ethan-Joiner <ethan.joiner@gmail.com>
2021-08-06 13:34:09 -0500

    i think this is it

--- src/util/warmup.js
+++ src/util/warmup.js
@@ -804,14 +804,10 @@ function strangeGrid(r, c) {
     let evenRowAdds = [1,3,5,7,9];
     let oddRowAdds = [0,2,4,6,8];
     let answer = 0;
-    if(r == 2){
-        answer = evenRowAdds[c - 1];
-    } else if (r == 1){
-        answer = oddRowAdds[c - 1];
-    } else if(r % 2 == 0) {
-        answer = row / 2 + evenRowAdds[c - 1];
+    if(r % 2 == 0) {
+        answer = ((r / 2) - 1) * 10 + evenRowAdds[c - 1];
     } else {
-        answer = Math.floor(row / 2) + oddRowsAdds[c - 1];
+        answer = ((Math.floor(row / 2) - 1) * 10) + oddRowsAdds[c - 1];
     }
     return answer;
 }
