95f3bef76f9a85603192fba58c6aa972ba43658c
Ethan-Joiner <ethan.joiner@gmail.com>
2021-08-25 12:55:37 -0500

    passed all

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -781,20 +781,20 @@ public static int lowestTriangle(int trianglebase, int area) {
     //     System.out.println(A.substring(0, 1).toUpperCase().concat(A.substring(1)) + " " + B.substring(0, 1).toUpperCase().concat(B.substring(1)));
 	// }
 
-	public static String getSmallestAndLargest(String s, int k) {
+	// public static String getSmallestAndLargest(String s, int k) {
 
-		String smallest = s.substring(0,k);
-		String largest = s.substring(0,k);
-		for(int i = 0; i < s.length() - k; i++){
-			if(smallest.compareTo(s.substring(i,i+k)) < 0){
-				smallest = s.substring(i,i+k);
-			}
-			if(largest.compareTo(s.substring(i,i+k)) > 0){
-				largest = s.substring(i,i+k);
-			}
-		}
-		return smallest + "\n" + largest;	
-    }
+	// 	String smallest = s.substring(0,k);
+	// 	String largest = s.substring(0,k);
+	// 	for(int i = 0; i < s.length() - k; i++){
+	// 		if(smallest.compareTo(s.substring(i,i+k)) > 0){
+	// 			smallest = s.substring(i,i+k);
+	// 		}
+	// 		if(largest.compareTo(s.substring(i,i+k)) < 0){
+	// 			largest = s.substring(i,i+k);
+	// 		}
+	// 	}
+	// 	return smallest + "\n" + largest;	
+    // }
 }
 
 
