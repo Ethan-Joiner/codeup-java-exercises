b3f1abbe97f21dd88b821d11d1e795c49078bcc0
Ethan-Joiner <ethan.joiner@gmail.com>
2021-08-25 11:59:49 -0500

    passed all ,on to smallest and largest substring

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -764,21 +764,32 @@ public static int lowestTriangle(int trianglebase, int area) {
 		// 	Calendar.DAY_OF_WEEK_IN_MONTH(4);
 		// }
 
-		public static void Solution(){
+	// 	public static void Solution(){
 			
 		
-		Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
+	// 	Scanner sc=new Scanner(System.in);
+    //     String A=sc.next();
+    //     String B=sc.next();
 
-		System.out.println(A.length() + B.length());
-        if(A.charAt(0) > B.charAt(0)){
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
-        System.out.println(A.substring(0, 1).toUpperCase());
-	}
+	// 	System.out.println(A.length() + B.length());
+    //     if(A.charAt(0) > B.charAt(0)){
+    //         System.out.println("Yes");
+    //     } else {
+    //         System.out.println("No");
+    //     }
+    //     System.out.println(A.substring(0, 1).toUpperCase().concat(A.substring(1)) + " " + B.substring(0, 1).toUpperCase().concat(B.substring(1)));
+	// }
+
+	public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
 }
 
 
