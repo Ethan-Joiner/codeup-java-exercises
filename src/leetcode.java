12f5cdece5bca26977617a3c034337e27dd785d4
Ethan-Joiner <ethan.joiner@gmail.com>
2022-03-29 12:28:27 -0500

    attempt 2

--- src/leetcode.java
+++ src/leetcode.java
@@ -57,8 +57,8 @@ public class leetcode {
 public int[] twoSum(int[] nums, int target) {
     int[] ans = new int[2];
     for(int i = 0; i < nums.length; i++){
-        for(int j = 0; i < nums.length; j++){
-            if(i + j == target){
+        for(int j = 1; i < nums.length; j++){
+            if(nums[i] + nums[j] == target){
                 ans[0] = i;
                 ans[1] = j;
                 break;
