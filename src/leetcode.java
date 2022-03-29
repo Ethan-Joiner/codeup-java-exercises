d83b4e6d73189540e79af62637a5f6b276bbca8c
Ethan-Joiner <ethan.joiner@gmail.com>
2022-03-29 11:58:11 -0500

    forgot to clean unused variables

--- src/leetcode.java
+++ src/leetcode.java
@@ -8,15 +8,6 @@ public class leetcode {
 
     public static int romanToInt(String s) {
         int ans = 0;
-        int I = 1;
-        int IV = 4;
-        int V = 5;
-        int IX = 9;
-        int X = 10;
-        int L = 50;
-        int C = 100;        
-        int D = 500;    
-        int M = 1000;
         for(int i = 0; i < s.length(); i++){
             if(i < s.length() - 1 && s.charAt(i) == 'C' && s.charAt(i +1) == 'M'){
                 ans += 900;
