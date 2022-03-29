9b9fdfa0f5ac4bcedeb03f18bd7007d6c84786d6
Ethan-Joiner <ethan.joiner@gmail.com>
2022-03-29 12:40:41 -0500

    this doesnt make any sense

--- src/leetcode.java
+++ src/leetcode.java
@@ -3,7 +3,9 @@ public class leetcode {
     
 
     public static void main(String[] args) {
-        romanToInt("MCMXCIV");
+        // romanToInt("MCMXCIV");
+        int[] testArray = {3,3};
+        twoSum(testArray, 6);
     }
 
     public static int romanToInt(String s) {
@@ -54,12 +56,14 @@ public class leetcode {
 
 // You can return the answer in any order.
 
-public int[] twoSum(int[] nums, int target) {
+public static int[] twoSum(int[] nums, int target) {
     int[] ans = new int[2];
     for(int i = 0; i < nums.length; i++){
         for(int j = 1; j < nums.length; j++){
             System.out.println("i in loop" + i);
             System.out.println("j in loop" + j);
+            System.out.println(nums[i]);
+            System.out.println(nums[j]);
             if(nums[i] + nums[j] == target){
                 ans[0] = i;
                 ans[1] = j;
