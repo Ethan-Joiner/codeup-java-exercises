089856f41612b47729fae69253f23935b9e9c3b4
Ethan-Joiner <ethan.joiner@gmail.com>
2022-04-27 10:15:21 -0500

    submitting for test cases

--- src/leetcode.java
+++ src/leetcode.java
@@ -240,6 +240,21 @@ public static int trap(int[] height) {
 
 // Find the index of the target in the array, or the index it would be if it does not exist. 
 public int searchInsert(int[] nums, int target) {
+    int ans = 0;
+    if(target > nums[nums.length - 1]){
+        return nums.length;
+    }
+    if(target < nums[0]){
+        return 0;
+    }
+    for(int i = 0; i < nums.length; i++){
+        if (i < nums.length - 1 && target < nums[i] && target > nums[i + 1]){
+            ans = i - 1;
+        } 
+
+        }
+        return ans;
+    }
+};
         
-}
-}
+
